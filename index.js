cf022bfff91b9872decbdb300a33514a32e583d7
MikeMike39 <Mfonseca294@gmail.com>
2021-03-27 14:53:28 -0400

    6 commit

---
+++ index.js
@@ -0,0 +1,13 @@
+//Choose a random color
+const button = document.querySelector("button");
+const body = document.querySelector("body");
+const colors = ["red", "green", "blue", "yellow", "pink", "purple"];
+//Starting background color before the click
+body.style.backgroundColor = "lightblue";
+//Event listener wait for user input
+button.addEventListener("click", changeBackground);
+// Function for random color
+function changeBackground() {
+  const colorIndex = parseInt(Math.random() * colors.length + 1);
+  body.style.backgroundColor = colors[colorIndex];
+}
